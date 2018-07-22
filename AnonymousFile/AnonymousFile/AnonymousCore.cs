2ffdca50a3cf8df1d68cfb9bfd2d9503965312c7
Viyrex(aka Yuyu) <0x0001F36D@gmail.com>
2018-07-22 19:50:43 +0800

    add AnonymousCore.cs and AnonymousFile.cs, Update AssemblyInfo

---
+++ AnonymousFile/AnonymousFile/AnonymousCore.cs
@@ -0,0 +1,88 @@
+﻿// Author: Viyrex(aka Yuyu)
+// Contact: mailto:viyrex.aka.yuyu@gmail.com
+// Github: https://github.com/0x0001F36D
+
+namespace Anonymous
+{
+    using System;
+
+    /// <summary>
+    /// 提供透明字串與 <see langword="int"/> 型別間的核心轉換函式
+    /// </summary>
+    public static class AnonymousCore
+    { 
+        #region Fields
+
+        private const char HIGH_BIT_CHARACTER = (char)0xFFFF;
+        private const char LOW__BIT_CHARACTER = (char)0xFFFE;
+
+        #endregion Fields
+
+        #region Methods
+
+        /// <summary>
+        /// 透過識別碼轉換為對應之透明字串
+        /// </summary>
+        /// <param name="id">識別碼</param>
+        /// <returns>透明字串</returns>
+        public static string Convert(int id)
+        {
+            var target = new char[32];
+            for (int i = 31; i >= 0; i--)
+                target[i] = Convert((id & (1 << i)) != 0);
+
+            return new string(target);
+        }
+
+        /// <summary>
+        /// 從透明字串轉換為對應的識別碼
+        /// </summary>
+        /// <param name="str">透明字串</param>
+        /// <returns>識別碼</returns>
+        /// <exception cref="ArgumentOutOfRangeException"/>
+        /// <exception cref="NotSupportedException"/>
+        public static int Convert(string str)
+        {
+            if (str.Length > 31)
+                throw new ArgumentOutOfRangeException("Too many elements to be converted to a single int");
+
+            if (Validate(str))
+                throw new NotSupportedException("Find some illegal characters");
+
+            int val = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (Convert(str[i]))
+                {
+                    val |= 1 << i;
+                }
+            }
+            return val;
+        }
+
+        /// <summary>
+        /// 驗證字串是否為合法字串
+        /// </summary>
+        /// <param name="str">待驗證字串</param>
+        /// <returns>如果為合法字串則為 <see langword="true"/> ， 反之則為 <see langword="false"/></returns>
+        public static bool Validate(string str)
+        {
+            if (str is null)
+                return false;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] != HIGH_BIT_CHARACTER || str[i] != LOW__BIT_CHARACTER)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private static char Convert(bool b) => b ? HIGH_BIT_CHARACTER : LOW__BIT_CHARACTER;
+
+        private static bool Convert(char c) => c == HIGH_BIT_CHARACTER;
+
+        #endregion Methods
+    }
+}
---
+++ AnonymousFile/AnonymousFile/AnonymousFile.cs
@@ -0,0 +1,86 @@
+﻿// Author: Viyrex(aka Yuyu)
+// Contact: mailto:viyrex.aka.yuyu@gmail.com
+// Github: https://github.com/0x0001F36D
+
+namespace Anonymous
+{
+    using System.IO;
+
+    /// <summary>
+    /// 提供對匿名檔案的部分操作支援
+    /// </summary>
+    public sealed class AnonymousFile
+    {
+        #region Constructors
+
+        /// <summary>
+        /// 初始化 <see cref="AnonymousFile"/> 類別的新執行個體
+        /// </summary>
+        /// <param name="directory">目錄路徑</param>
+        public AnonymousFile(string directory)
+        {
+            Directory.CreateDirectory(directory);
+            this._path = directory + "/{0}";
+        }
+
+        #endregion Constructors
+
+        #region Fields
+
+        private readonly string _path;
+
+        #endregion Fields
+
+        #region Methods
+
+        /// <summary>
+        /// 建立或覆寫匿名檔案
+        /// </summary>
+        /// <param name="id">識別碼</param>
+        /// <returns>檔案流</returns>
+        public FileStream Create(int id)
+        {
+            return File.Create(this.CombinePath(id));
+        }
+
+        /// <summary>
+        /// 刪除匿名檔案
+        /// </summary>
+        /// <param name="id">識別碼</param>
+        public void Delete(int id)
+        {
+            var name = this.CombinePath(id);
+            if (File.Exists(name))
+                File.Delete(name);
+        }
+
+        /// <summary>
+        /// 取回匿名檔案
+        /// </summary>
+        /// <param name="id">識別碼</param>
+        /// <param name="file">檔案流</param>
+        /// <param name="mode">檔案流的開啟方式</param>
+        /// <param name="access">檔案流的存取權限</param>
+        /// <param name="share">檔案流的分享方式</param>
+        /// <returns>如果檔案存在則回傳 <see langword="true"/> ，反之則為 <see langword="false"/></returns>
+        public bool Retrieve(int id, out FileStream file, FileMode mode = FileMode.OpenOrCreate, FileAccess access = FileAccess.ReadWrite, FileShare share = FileShare.ReadWrite)
+        {
+            file = default;
+            var name = this.CombinePath(id);
+            var fi = new FileInfo(name);
+            if (fi.Exists)
+            {
+                file = fi.Open(mode, access, share);
+            }
+
+            return fi.Exists;
+        }
+
+        private string CombinePath(int id)
+        {
+            return string.Format(this._path, AnonymousCore.Convert(id));
+        }
+
+        #endregion Methods
+    }
+}
